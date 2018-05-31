2f748b8190438ced583523c15bfc8011982c7b51
hywell <hywell.28@gmail.com>
2018-05-31 10:36:08 +0800

    Site updated: 2018-05-31 10:35:58

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu May 31 2018 10:28:59 GMT+0800 (中国标准时间)
+// build time:Thu May 31 2018 10:35:57 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
