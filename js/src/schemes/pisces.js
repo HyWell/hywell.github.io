b29268324728338d5784f789f31f060f20e5ad4d
hywell <hywell.28@gmail.com>
2018-11-06 14:52:57 +0800

    Site updated: 2018-11-06 14:52:57

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Tue Nov 06 2018 11:34:42 GMT+0800 (中国标准时间)
+// build time:Tue Nov 06 2018 14:52:56 GMT+0800 (中国标准时间)
 $(document).ready(function(){var t=$(".sidebar-inner");e();i();function e(){var e=n(),i=f(),o=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight(),a=$("#content").height();if(e+o<a){t.affix({offset:{top:e-CONFIG.sidebar.offset,bottom:i}})}r(e).css({"margin-left":"initial"})}function i(){var t=window.matchMedia("(min-width: 991px)");t.addListener(function(t){if(t.matches){o()}})}function n(){return $(".header-inner").height()+CONFIG.sidebar.offset}function f(){var t=$(".footer-inner"),e=t.outerHeight(true)-t.outerHeight(),i=t.outerHeight(true)+e;return i}function r(t){return $("#sidebar").css({"margin-top":t})}function o(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");e()}});
 //rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Nov 06 2018 11:34:42 GMT+0800 (中国标准时间)
+// build time:Tue Nov 06 2018 14:52:56 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
