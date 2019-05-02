bb5154f3af0122cdf7db33ec6b8e849cbd42203a
hywell <hywell.28@gmail.com>
2019-05-02 12:55:40 +0800

    Site updated: 2019-05-02 12:55:39

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 25 2019 20:41:37 GMT+0800 (中国标准时间)
+// build time:Thu May 02 2019 12:54:34 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
